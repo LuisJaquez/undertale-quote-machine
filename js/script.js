c863afc17beebf4d33920c2a6f802852ea221312
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-02-22 21:06:28 -0400

    Set blockquote and append to quote section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 //Create Quote box elements
-const app = document.getElementById('quote-section');
-const blockquote = document.createElement('blockquote');
+const app = document.getElementById("quote-section");
+
+const blockquote = document.createElement("blockquote");
+blockquote.setAttribute("class", "quote-style");
+app.appendChild(blockquote);
