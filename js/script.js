93138184769cad4768b55d031ac82919b744307f
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-02-23 16:58:47 -0400

    Add function to get new quotes with and event listener

--- js/script.js
+++ js/script.js
@@ -7,23 +7,31 @@ p.setAttribute("class", "quote-style");
 app.appendChild(p);
 var buttonQuote = document.getElementById("quote-btn");
 
-//Request variable
-var request = new XMLHttpRequest();
+//Get new quotes
+function getQuote() {
+  //Request variable
+  var request = new XMLHttpRequest();
 
-//Set the API endpoint
-request.open(
-  "GET",
-  "https://random-quote-generator.herokuapp.com/api/quotes/random"
-);
+  //Set the API endpoint
+  request.open(
+    "GET",
+    "https://random-quote-generator.herokuapp.com/api/quotes/random"
+  );
 
-request.onload = function() {
-  var data = JSON.parse(this.response);
+  request.onload = function() {
+    var data = JSON.parse(this.response);
 
-  if (request.status >= 200 && request.status < 400) {
-    p.textContent = '\"' + data.quote + '\"' + " - " + data.author;
-  } else {
-    p.textContent = "Flowey it's the responsable :[";
-  }
-};
+    if (request.status >= 200 && request.status < 400) {
+      p.textContent = '"' + data.quote + '"' + " - " + data.author;
+    } else {
+      p.textContent = "Flowey it's the responsable :[";
+    }
+  };
 
-request.send();
+  request.send();
+}
+
+//To get the initial quote
+getQuote();
+//Events lisnteners
+buttonQuote.addEventListener("click", getQuote);
