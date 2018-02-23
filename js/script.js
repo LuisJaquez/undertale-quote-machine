c522332cc0f245628f6d28c0fc3ada8b547f2f08
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-02-22 23:46:15 -0400

    Set the API

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ var p = document.createElement("p");
 
 p.setAttribute("class", "quote-style");
 app.appendChild(p);
+var buttonQuote = document.getElementById("quote-btn");
+
+//Request variable
+var request = new XMLHttpRequest();
+
+request.open(
+  "GET",
+  "https://api.icndb.com/jokes/random?exclude=[explicit]"
+);
+
+request.withCredentials = true;
+
+
+request.onload = function() {
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    console.log(quoteText);
+  } else {
+    console.log("Error");
+  }
+};
+
+request.send();
